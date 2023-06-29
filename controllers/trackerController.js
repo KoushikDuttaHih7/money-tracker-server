8ec825adf03685cd9694effa474f2dc419d89a52
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-06-29 18:03:59 +0530

    Split Date and Time

--- controllers/trackerController.js
+++ controllers/trackerController.js
@@ -3,11 +3,14 @@ const TransactionModel = require("../models/trackerModel");
 exports.transaction = async (req, res) => {
   const { name, price, description, datetime } = req.body;
 
+  const date = datetime.split("T")[0];
+  const time = datetime.split("T")[1];
+
   const transaction = await TransactionModel.create({
     name,
     price,
     description,
-    datetime,
+    datetime: date + "," + time,
   });
 
   res.json(transaction);
